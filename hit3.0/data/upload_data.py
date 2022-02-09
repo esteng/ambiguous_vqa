aa303fd608d8e7043f0d6d119333d014c6adba83
esteng <elias.stengel@gmail.com>
2022-02-09 08:48:01 -0500

    upload data script

--- hit3.0/data/upload_data.py
+++ hit3.0/data/upload_data.py
@@ -13,15 +13,15 @@ def get_image_fname(num):
     name = f"COCO_val2014_{zeros}{num}.jpg"
     return name
 
-image_root = pathlib.Path("/brtx/603-nvme2/annotator_uncertainty/vqa/val2014") 
+image_root = pathlib.Path("/brtx/603-nvme2/estengel/annotator_uncertainty/vqa/train2014/") 
 
 to_copy_path = pathlib.Path("to_copy")
-to_copy_path.mkdir(exists_ok=True)
+to_copy_path.mkdir(exist_ok=True)
 
 for example in data:
-    image_id = example['image_id']
-    fname = get_image_fname(num) 
-    path_to_image = image_root.joinpath(fname) 
+    image_id = example['image']
+    # fname = get_image_fname(image_id) 
+    path_to_image = image_root.joinpath(image_id) 
    
     # copy 
     subprocess.Popen(["cp", str(path_to_image), "to_copy"])  
