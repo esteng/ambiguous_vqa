671e8ad1da2db6569c2e421efbf3ad1f0e8394c7
jguallar-blasco <33358958+jguallar-blasco@users.noreply.github.com>
2022-06-08 13:29:14 -0400

    added elia's corrected data, wrote append script

--- jimena_work/append_clean_csv_to_json.py
+++ jimena_work/append_clean_csv_to_json.py
@@ -9,8 +9,8 @@ import pdb
 from scipy.optimize import linear_sum_assignment
 
 """
-Sort out nom problematic exampels so we can re analyze
-problem examples
+Sort out non-problematic exampels so we can re-analyze
+problematic examples
 """
 
 def get_line(line):
@@ -36,17 +36,37 @@ def write_csv(to_write, out_path):
             writer.writerow(line)
 
 def sort(data):
+    delete_count = 0
+    flag_count = 0
+    delete_flag_count = 0
     sorted_data = []
     for line in data:
-        if line["Answer.skip_reason"] == '"flag"' or line["Answer.skip_reason"] == '"flag"':
-            sorted_data.append(line)
+        if line["Answer.skip_reason"] == '"flag"' or line["Answer.skip_reason"] == '"delete/flag"':
+            flag_count += 1
+            continue
+        if line["Answer.skip_reson"] == '"delete/flag"':
+            delete_flag_count += 1
+            continue
+        if line["Answer.skip_reason"] == '"delete/flag"' or line["Answer.skip_reason"] == '"delete"':
+            delete_count += 1
+            continue
+        sorted_data.append(line)
+
+    print("Data stats: ")
+    print("\tExamples delted: " + delete_count)
+    print("\tExamples flagged (kept and deleted): " + flag_count)
+    exit 
     to_write = [get_line(l) for l in sorted_data]
     write_csv(to_write, args.out_path)
 
 def main(args):
     data = []
-    with open(args.input_csv) as read_obj:
-        csv_reader = csv.DictReader(read_obj)
+    with open(args.input_1_csv) as read_obj_1:
+        csv_reader = csv.DictReader(read_obj_1)
+        for row in csv_reader:
+            data.append(row)
+    with open(args.input_2_csv) as read_obj_2:
+        csv_reder = csv.DictReader(read_obj_2)
         for row in csv_reader:
             data.append(row)
     sort(data)
@@ -54,7 +74,8 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input-csv", type=str, dest='input_csv', required=True)
+    parser.add_argument("--input-1-csv", type=str, dest='input_1_csv', required=True)
+    parser.add_argument("--input-2-csv", type=str, dest='input_2_csv', required=True)
     parser.add_argument("--out-path", type=str, dest='out_path', required=True)
     args = parser.parse_args()
 
