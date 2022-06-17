e5833570534ee8e0fb0ed70238916d58e78f511c
jguallar-blasco <33358958+jguallar-blasco@users.noreply.github.com>
2022-06-17 15:02:54 -0400

    new tests

--- analysis/abstract_class_test.py
+++ analysis/abstract_class_test.py
@@ -2,8 +2,28 @@ from abstract_class import *
 
 def test_BLEU():
     # Bad input -> no result
-    assert BLEU_similarity_score()
+    # The dog sleeps well vs. The dog jumps well
+    assert BLEU_similarity_score('I am good', 'You are good') == 1.4637115948630222e-231 # 0N
+    assert BLEU_similarity_score('I am good', 'You are good', 1) == 0.416667
+    assert BLEU_similarity_score('I am good', 'You are good', 2) == 0.000000 # 0N
+    assert BLEU_similarity_score('I am good', 'You are good', 3) == 0.000000
+    assert BLEU_similarity_score('I am good', 'You are good', 4) == 0.000000
 
+    # I am good vs. You are good
+    assert BLEU_similarity_score('I am good', 'You are good') == 1.4637115948630222e-231 # 0N
+    assert BLEU_similarity_score('I am good', 'You are good', 1) == 0.416667
+    assert BLEU_similarity_score('I am good', 'You are good', 2) == 0.000000 # 0N
+    assert BLEU_similarity_score('I am good', 'You are good', 3) == 0.000000
+    assert BLEU_similarity_score('I am good', 'You are good', 4) == 0.000000
+
+    # Their mosquito nets did not protect vs. The behives did a lot of damage
+    assert BLEU_similarity_score('I am good', 'You are good') == 1.3729916628506288e-231 # 0N
+    assert BLEU_similarity_score('I am good', 'You are good', 1) == 0.322581
+    assert BLEU_similarity_score('I am good', 'You are good', 2) == 0.000000 # 0N
+    assert BLEU_similarity_score('I am good', 'You are good', 3) == 0.000000
+    assert BLEU_similarity_score('I am good', 'You are good', 4) == 0.000000
+
+   
     # Good input -> results
 
 def test_BERT():
