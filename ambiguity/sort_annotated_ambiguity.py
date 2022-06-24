cf7bccdc52516fa7d8327878c21941149f030b0d
jguallar-blasco <33358958+jguallar-blasco@users.noreply.github.com>
2022-06-24 15:13:39 -0400

    added main function

--- ambiguity/sort_annotated_ambiguity.py
+++ ambiguity/sort_annotated_ambiguity.py
@@ -7,3 +7,21 @@ from csv import reader
 import pdb
 from scipy.optimize import linear_sum_assignment
 import copy
+
+def sort(data):
+    
+
+def main(args):
+    data = []
+    with open(args.input_csv) as read_obj:
+        csv_reader = csv.DictReader(read_obj)
+        for row in csv_reader:
+            data.append(row)
+    
+    sort(data)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--inut-csv", type=str, dest='input_csv', required=True)
+
+    main(args)
