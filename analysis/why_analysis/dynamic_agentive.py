155a5af7703f1a36ad77b2bb89242ece3a1533f1
esteng <elias.stengel@gmail.com>
2022-08-15 11:11:48 -0400

    annotate dynamic

---
+++ analysis/why_analysis/dynamic_agentive.py
@@ -0,0 +1,43 @@
+import csv 
+import pathlib
+import json 
+import argparse
+
+def read_csv(path):
+    all_data = []
+    with open(path) as f1:
+        reader = csv.DictReader(f1)
+        for row in reader:
+            data = {k:json.loads(v) for k, v in row.items()}
+            all_data.append(data)
+    return all_data
+
+def annotate(data, resume_path): 
+    if resume_path.exists():
+        with open(resume_path) as f1:
+            data = json.load(f1)
+    for i, row in enumerate(data):
+        if 'is_dynamic' in row.keys() and row['is_dynamic'] is not None:
+            # already annotated 
+            continue
+        print(row['questionStr'])
+        is_dynamic = input("\tDynamic? ")
+        is_agentive = input("\tAgentive? ")
+        row['is_dynamic'] = is_dynamic
+        row['is_agentive'] = is_agentive
+        data[i] = row
+        save_annotations(data, resume_path)
+
+def save_annotations(data, path):
+    with open(path) as f1:
+        json.dump(data, f1)
+
+if __name__ == "__main__": 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--csv", type=str, default="output.csv")
+    parser.add_argument("--out-file", type=str, default = "dynamic_agentive.json")
+    args = parser.parse_args()
+
+    data = read_csv(args.csv)
+    out_file = pathlib.Path(args.out_file)
+    annotate(data, out_file)
