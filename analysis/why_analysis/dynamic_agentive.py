6776038e178556bceda248b4cc5044071c16f813
esteng <elias.stengel@gmail.com>
2022-08-15 15:19:50 -0400

    why analysis

--- analysis/why_analysis/dynamic_agentive.py
+++ analysis/why_analysis/dynamic_agentive.py
@@ -8,7 +8,13 @@ def read_csv(path):
     with open(path) as f1:
         reader = csv.DictReader(f1)
         for row in reader:
-            data = {k:json.loads(v) for k, v in row.items()}
+            data = {}
+            for k, v in row.items():
+                try:
+                    v = json.loads(v)
+                except json.JSONDecodeError:
+                    v = v
+                data[k] = v
             all_data.append(data)
     return all_data
 
@@ -20,7 +26,7 @@ def annotate(data, resume_path):
         if 'is_dynamic' in row.keys() and row['is_dynamic'] is not None:
             # already annotated 
             continue
-        print(row['questionStr'])
+        print(row['Input.questionStr'])
         is_dynamic = input("\tDynamic? ")
         is_agentive = input("\tAgentive? ")
         row['is_dynamic'] = is_dynamic
@@ -29,8 +35,8 @@ def annotate(data, resume_path):
         save_annotations(data, resume_path)
 
 def save_annotations(data, path):
-    with open(path) as f1:
-        json.dump(data, f1)
+    with open(str(path), "w") as f1:
+        json.dump(data, f1, indent=4)
 
 if __name__ == "__main__": 
     parser = argparse.ArgumentParser()
